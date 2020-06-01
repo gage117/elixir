e036aa18dc835cbecb8498a685c24ad83c19d7f5
Gage Eide <gageeide@gmail.com>
2020-06-01 13:55:53 -0500

    Put Platform IDs in separate file, platform_IDs.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
 const API = require('./api');
+const platform_IDs = require('./platform_IDs');
 
 const app = express();
 
@@ -44,49 +45,7 @@ app.get('/genres', async (req, res) => {
 });
 
 app.get('/platforms', async (req, res) => {
-  const IDs = [
-    163, //* SteamVR 
-    92, //* Steam
-    3, //* Linux
-    6, //* PC (Microsoft Windows)
-    162, //* Oculus VR
-    7, //* PlayStation 1
-    8, //* PlayStation 2
-    9, //* PlayStation 3
-    48, //* PlayStation 4
-    167, //* PlayStation 5
-    165, //* PlayStation VR
-    46, //* PlayStation Vita
-    38, //* PSP (PlayStation Portable)
-    34, //* Android
-    39, //* iOS
-    203, //* Google Stadia
-    170, //* Google Stadia Again.
-    11, //* Xbox
-    12, //* Xbox 360
-    49, //* Xbox One
-    169, //* Xbox Series X
-    18, //* NES (Nintendo Entertainment System)
-    19, //* SNES (Super Nintendo Entertainment System)
-    4, //* Nintendo 64
-    33, //* Nintendo Game Boy
-    22, //* Game Boy Color
-    24, //* Game Boy Advance
-    21, //* Nintendo GameCube
-    20, //* Nintendo DS
-    159, //* Nintendo DSi
-    37, //* Nintendo 3DS
-    137, //* New Nintendo 3DS
-    5, //* Nintendo Wii
-    41, //* Nintendo WiiU
-    130, //* Nintendo Switch
-    42, //* NGage (Because why wouldn't I include the console that has my name in it?)
-    29, //* Sega Genesis
-    32, //* Sega Saturn
-    64, //* Sega Master System
-    59, //* Atari 2600
-  ]
-  const queryString = `fields *; limit 500; where id = (${IDs.join(',')});`;
+  const queryString = `fields *; limit 500; where id = (${platform_IDs.join(',')});`;
   const response = await API.post('/platforms', queryString);
   res.send(response.data);
 });
---
+++ src/platform_IDs.js
@@ -0,0 +1,44 @@
+const IDs = [
+    163, //* SteamVR 
+    92, //* Steam
+    3, //* Linux
+    6, //* PC (Microsoft Windows)
+    162, //* Oculus VR
+    7, //* PlayStation 1
+    8, //* PlayStation 2
+    9, //* PlayStation 3
+    48, //* PlayStation 4
+    167, //* PlayStation 5
+    165, //* PlayStation VR
+    46, //* PlayStation Vita
+    38, //* PSP (PlayStation Portable)
+    34, //* Android
+    39, //* iOS
+    203, //* Google Stadia
+    170, //* Google Stadia Again.
+    11, //* Xbox
+    12, //* Xbox 360
+    49, //* Xbox One
+    169, //* Xbox Series X
+    18, //* NES (Nintendo Entertainment System)
+    19, //* SNES (Super Nintendo Entertainment System)
+    4, //* Nintendo 64
+    33, //* Nintendo Game Boy
+    22, //* Game Boy Color
+    24, //* Game Boy Advance
+    21, //* Nintendo GameCube
+    20, //* Nintendo DS
+    159, //* Nintendo DSi
+    37, //* Nintendo 3DS
+    137, //* New Nintendo 3DS
+    5, //* Nintendo Wii
+    41, //* Nintendo WiiU
+    130, //* Nintendo Switch
+    42, //* NGage (Because why wouldn't I include the console that has my name in it?)
+    29, //* Sega Genesis
+    32, //* Sega Saturn
+    64, //* Sega Master System
+    59, //* Atari 2600
+];
+
+module.exports = IDs;
