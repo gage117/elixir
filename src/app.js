d20514fc5a3bfa9ffb19e24611218b30deca830c
Gage Eide <gageeide@gmail.com>
2020-06-01 16:39:07 -0500

    Fixed app-load

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.get('/app-load', async (req, res) => {
   //* Object to assign API response data to and send back in response
   const appData = {};
   //* Get data from /games
-  let queryString = `fields *, cover.url, platforms; limit 25; where platforms !=n & platforms = (${major_platforms.join(',')}) & parent_game = null;`;
+  let queryString = `fields *, cover.url, platforms; limit 25; where platforms !=n & platforms = (${major_platforms.join(',')}) & parent_game = null & total_rating >= 75 & total_rating_count >= 100; sort total_rating desc;`;
   const gamesResponse = await API.post('/games', queryString);
   appData.gameData = gamesResponse.data;
   //* Get data from /genres
